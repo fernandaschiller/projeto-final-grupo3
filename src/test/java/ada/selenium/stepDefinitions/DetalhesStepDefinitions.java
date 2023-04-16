98d3be655b1b7a73cd4ac85dc7595b41d7f737e9
Jandson1 <eng.jandsonmoura@gmail.com>
2023-04-16 13:15:16 -0300

    Criando Branch de Detalhes

---
+++ src/test/java/ada/selenium/stepDefinitions/DetalhesStepDefinitions.java
@@ -0,0 +1,11 @@
+package ada.selenium.stepDefinitions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DetalhesStepDefinitions {
+    public ChromeDriver driver;
+    public DetalhesStepDefinitions(){driver = new ChromeDriver();}
+
+}
