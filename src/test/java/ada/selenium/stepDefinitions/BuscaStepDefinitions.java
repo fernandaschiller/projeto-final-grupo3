049248bbc831586d7aec29c005983481237855a9
Fernanda Schiller <schillerfe@gmail.com>
2023-04-19 23:55:24 +0100

    add: cenários de busca

--- src/test/java/ada/selenium/stepDefinitions/BuscaStepDefinitions.java
+++ src/test/java/ada/selenium/stepDefinitions/BuscaStepDefinitions.java
@@ -5,6 +5,8 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -22,9 +24,70 @@ public class BuscaStepDefinitions {
         driver.quit();
     }
 
-    @Given("^que o usuário acesse a página '(.+)'$")
-    public void que_usuario_acessa_pagina_home(String url){
+    @Given("^que o usuário acesse a página home '(.+)'$")
+    public void que_usuario_acessa_pagina_home(String url) throws InterruptedException {
         driver.get(url);
+        Thread.sleep(2000);
         Assertions.assertTrue(driver.getTitle().contains("Saraiva"));
+        driver.findElement(By.cssSelector("[class^=\"_lgpdy-banner _lgpdy-v-bottom\"] [class^=\"_lgpdy-cta-accept\"]")).click();
+    }
+
+    @Then("deve exibir caixa de pesquisa")
+    public void deve_exibir_caixa_pesquisa() {
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[class^=\"vtex-input-prefix__group\"]")).isDisplayed());
+    }
+
+    @When("usuário clicar na caixa de pesquisa")
+    public void usuario_clicar_caixa_de_pesquisa() throws InterruptedException {
+        driver.findElement(By.cssSelector("[class^=vtex-input] input")).click();
+        Thread.sleep(2000);
+    }
+
+    @And("^usuário digitar '(.+)' na caixa de pesquisa$")
+    public void usuario_digitar_produto_caixa_pesquisa(String produto) throws InterruptedException {
+        driver.findElement(By.cssSelector("[class^=vtex-input] input")).sendKeys(produto + Keys.ENTER);
+        Thread.sleep(5000);
+    }
+
+    @Then("deve exibir uma tela com os resultados relevantes da busca realizada na mesma guia")
+    public void deve_exibir_tela_com_resultados() {
+        Assertions.assertTrue(driver.findElement(By.id("gallery-layout-container")).isDisplayed());
+    }
+
+    @Then("deve exibir sugestão dos termos mais buscados")
+    public void deve_exibir_sugestao_dos_termos_mais_buscados() {
+        Assertions.assertTrue(driver.findElement(By.cssSelector("section[class^=\"vtex-search-2-x-biggy-autocomplete\"]")).isDisplayed());
+    }
+
+    @Then("deve exibir uma mensagem de erro informando que nenhum resultado foi encontrado")
+    public void deve_exibir_mensagem_de_erro_informando_que_nenhum_resultado_foi_encontrado() {
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[class^=\"vtex-search-result-3-x-searchNotFoundOops\"]")).isDisplayed());
+    }
+
+    @And("deve ser possível verificar a quantidade de produtos retornados, no canto superior direito da tela")
+    public void deve_ser_possivel_verificar_quantidade_produtos_retornados() {
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[class^=\"vtex-search-result-3-x-totalProductsMessage\"]")).isDisplayed());
+    }
+
+    @And("a opção de filtro deve estar disponível")
+    public void opcao_de_filtro_deve_estar_disponivel() throws InterruptedException {
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[class^=\"vtex-search-result-3-x-filterMessage\"]")).isDisplayed());
+    }
+
+    @And("^usuário digitar '(.+)' na caixa de pesquisa mas não apertar enter$")
+    public void usuario_digitar_produto_caixa_pesquisa_sem_enter(String produto) throws InterruptedException {
+        driver.findElement(By.cssSelector("[class^=vtex-input] input")).sendKeys(produto);
+        Thread.sleep(5000);
+    }
+
+    @Then("deve aparecer a mensagem 'SEM SUGESTÕES'")
+    public void deve_aparecer_sem_sugestoes() {
+        Assertions.assertTrue(driver.findElement(By.cssSelector("[class^=\"vtex-search-2-x-itemListTitle\"]")).isDisplayed());
+    }
+
+    @And("deve ser possível clicar em um item sugerido nos termos mais buscados")
+    public void deve_ser_possível_clicar_no_item_sugerido() throws InterruptedException {
+        driver.findElement(By.cssSelector("ol li:nth-child(1)")).click();
+        Thread.sleep(2000);
     }
 }
---
+++ src/test/resources/ada/selenium/busca.feature
@@ -0,0 +1,65 @@
+Feature: Busca de produtos
+
+  Como usuário, mesmo que não esteja logado,
+  Quero realizar busca por produtos
+  Para encontrar o produto que tenho interesse
+
+  r1: Ao clicar no item de busca e não digitar um texto, deve exibir uma sugestão dos termos mais buscados
+  r2: Ao efetuar busca de produto, deve redirecionar para uma tela de resultados da busca realizada (na mesma guia)
+  r3: Na tela do resultado da busca deve informar a quantidade de produtos retornados e deve ser possível realizar filtros dos produtos
+
+  @HG3R1
+  Scenario: Validar presença da caixa de pesquisa
+    Given que o usuário acesse a página home 'https://www.saraiva.com.br/'
+    Then deve exibir caixa de pesquisa
+
+  Scenario: Validar sugestão dos termos mais buscados no item de busca da página
+    Given que o usuário acesse a página home 'https://www.saraiva.com.br/'
+    When usuário clicar na caixa de pesquisa
+    Then deve exibir sugestão dos termos mais buscados
+
+  Scenario: Validar busca de produto
+    Given que o usuário acesse a página home 'https://www.saraiva.com.br/'
+    When usuário clicar na caixa de pesquisa
+    And usuário digitar 'Harry Potter' na caixa de pesquisa
+    Then deve exibir uma tela com os resultados relevantes da busca realizada na mesma guia
+
+  Scenario: Validar seleção de um produto que aparece nos termos mais buscados
+    Given que o usuário acesse a página home 'https://www.saraiva.com.br/'
+    When usuário clicar na caixa de pesquisa
+    Then deve exibir sugestão dos termos mais buscados
+    And deve ser possível clicar em um item sugerido nos termos mais buscados
+    And deve exibir uma tela com os resultados relevantes da busca realizada na mesma guia
+
+  Scenario: Validar quantidade de produtos retornados e possibilidade de aplicar filtros
+    Given que o usuário acesse a página home 'https://www.saraiva.com.br/'
+    When usuário clicar na caixa de pesquisa
+    And usuário digitar 'Harry Potter' na caixa de pesquisa
+    Then deve exibir uma tela com os resultados relevantes da busca realizada na mesma guia
+    And deve ser possível verificar a quantidade de produtos retornados, no canto superior direito da tela
+    And a opção de filtro deve estar disponível
+
+  Scenario: Validar ordenação da página de resultados por menor preço
+    #Given que o usuário está na página de resultados da pesquisa
+    #When o usuário selecionar a ordenação "Preço: Menor preço"
+    #Then a lista de resultados deve ser reordenada por preço, do menor para o maior
+
+  Scenario: Validar ordenação da página de resultados por maior preço
+    #Given que o usuário está na página de resultados da pesquisa
+    #When o usuário selecionar a ordenação "Preço: Maior preço"
+    #Then a lista de resultados deve ser reordenada por preço, do maior para o menor
+
+  Scenario: Validar busca de produto não encontrado
+    Given que o usuário acesse a página home 'https://www.saraiva.com.br/'
+    When usuário clicar na caixa de pesquisa
+    And usuário digitar 'xxxx1234' na caixa de pesquisa
+    Then deve exibir uma mensagem de erro informando que nenhum resultado foi encontrado
+
+  Scenario: Validar busca sem sugestão
+    Given que o usuário acesse a página home 'https://www.saraiva.com.br/'
+    When usuário clicar na caixa de pesquisa
+    And usuário digitar 'xxxx1234' na caixa de pesquisa mas não apertar enter
+    Then deve aparecer a mensagem 'SEM SUGESTÕES'
+
+
+
